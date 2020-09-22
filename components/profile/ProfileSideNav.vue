0ccf31a2be3da709cae5538a3251ede35e651699
Ibrohim Hanifa <31683290+ibrohimhanifa@users.noreply.github.com>
2020-09-22 10:07:46 +0700

    logo

--- components/profile/ProfileSideNav.vue
+++ components/profile/ProfileSideNav.vue
@@ -1,23 +1,11 @@
 <template>
   <!-- Side Nav Bar-->
   <aside class="flex flex-col items-center bg-white text-gray-700 h-full">
-    <div class="h-16 flex items-center w-full">
-      <nuxt-link class="h-6 w-6 mx-auto" to="/home">
-        <!-- logo -->
-        <svg
-          class="fill-current h-8 w-8 mr-2"
-          width="54"
-          height="54"
-          viewBox="0 0 54 54"
-          xmlns="http://www.w3.org/2000/svg"
-        >
-          <path
-            d="M13.5 22.1c1.8-7.2 6.3-10.8 13.5-10.8 10.8 0 12.15 8.1 17.55 9.45 3.6.9 6.75-.45 9.45-4.05-1.8 7.2-6.3 10.8-13.5 10.8-10.8 0-12.15-8.1-17.55-9.45-3.6-.9-6.75.45-9.45 4.05zM0 38.3c1.8-7.2 6.3-10.8 13.5-10.8 10.8 0 12.15 8.1 17.55 9.45 3.6.9 6.75-.45 9.45-4.05-1.8 7.2-6.3 10.8-13.5 10.8-10.8 0-12.15-8.1-17.55-9.45-3.6-.9-6.75.45-9.45 4.05z"
-          />
-        </svg>
-        <!-- logo -->
-      </nuxt-link>
-    </div>
+    <nuxt-link class="h-16 w-full mx-auto flex justify-center items-center" to="/home">
+      <!-- logo -->
+      <img src="~/assets/img/logo.png" class="fill-current h-8 mt-3" />
+      <!-- logo -->
+    </nuxt-link>
 
     <ul>
       <!-- Items Profile -->
