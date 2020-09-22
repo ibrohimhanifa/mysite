c30ef2841fdf4ac16e63740be30d9c00a36b860f
Ibrohim Hanifa <31683290+ibrohimhanifa@users.noreply.github.com>
2020-09-23 00:31:03 +0700

    last

--- components/index/Hero.vue
+++ components/index/Hero.vue
@@ -2,13 +2,34 @@
   <div class="m-auto max-w-6xl p-12">
     <div class="flex flex-col md:flex-row justify-evenly">
       <div class="md:w-1/2 max-w-md flex flex-col justify-center">
-        <div class="md:text-5xl text-2xl uppercase font-black">Ibrohim Hanifa</div>
+        <div class="md:text-5xl text-2xl uppercase font-black text-gray-900">Ibrohim Hanifa</div>
         <div class="flex items-center">
-          <img class="h-5 mr-2" src="~/assets/icon/solid/mail.svg" alt />
+          <svg
+            class="h-5 mr-2 text-gray-900"
+            xmlns="http://www.w3.org/2000/svg"
+            viewBox="0 0 20 20"
+            fill="currentColor"
+          >
+            <path d="M2.003 5.884L10 9.882l7.997-3.998A2 2 0 0016 4H4a2 2 0 00-1.997 1.884z" />
+            <path d="M18 8.118l-8 4-8-4V14a2 2 0 002 2h12a2 2 0 002-2V8.118z" />
+          </svg>
+
           <div class="md:text-lg text-md font-semibold text-gray-00">hanifa.ibrohim@gmail.com</div>
         </div>
         <div class="flex items-center">
-          <img class="h-5 mr-2" src="~/assets/icon/solid/location-marker.svg" alt />
+          <svg
+            class="h-5 mr-2 text-gray-900"
+            xmlns="http://www.w3.org/2000/svg"
+            viewBox="0 0 20 20"
+            fill="currentColor"
+          >
+            <path
+              fill-rule="evenodd"
+              d="M5.05 4.05a7 7 0 119.9 9.9L10 18.9l-4.95-4.95a7 7 0 010-9.9zM10 11a2 2 0 100-4 2 2 0 000 4z"
+              clip-rule="evenodd"
+            />
+          </svg>
+
           <div class="md:text-lg text-md font-semibold text-gray-900">Jakarta, ID</div>
         </div>
         <div class="my-8 h-16">
--- components/profile/ProfileNav.vue
+++ components/profile/ProfileNav.vue
@@ -3,22 +3,31 @@
   <aside class="flex flex-col items-center bg-white text-gray-700 h-full">
     <nuxt-link class="h-16 w-full mx-auto flex justify-center items-center" to="/home">
       <!-- logo -->
-      <img src="~/assets/img/logo.png" class="fill-current h-8 mt-3" />
+      <svg
+        xmlns="http://www.w3.org/2000/svg"
+        class="bg-gray-900 text-white h-8 mt-3 p-2 rounded-md fill-current"
+        viewBox="0 0 20 20"
+        fill="currentColor"
+      >
+        <path
+          fill-rule="evenodd"
+          d="M15.707 15.707a1 1 0 01-1.414 0l-5-5a1 1 0 010-1.414l5-5a1 1 0 111.414 1.414L11.414 10l4.293 4.293a1 1 0 010 1.414zm-6 0a1 1 0 01-1.414 0l-5-5a1 1 0 010-1.414l5-5a1 1 0 011.414 1.414L5.414 10l4.293 4.293a1 1 0 010 1.414z"
+          clip-rule="evenodd"
+        />
+      </svg>
       <!-- logo -->
     </nuxt-link>
 
     <ul>
       <!-- Items Profile -->
-      <li class="hover:bg-indigo-100">
+      <li>
         <nuxt-link
           to="/profile"
-          class="h-16 px-6 flex justify-center items-center w-full focus:text-indigo-500"
+          class="h-16 px-6 flex justify-center items-center w-full focus:text-gray-500"
         >
           <svg
-            class="h-5 w-5"
-            width="24"
-            height="24"
             xmlns="http://www.w3.org/2000/svg"
+            class="nav"
             fill="none"
             viewBox="0 0 24 24"
             stroke="currentColor"
@@ -26,22 +35,20 @@
             <path
               stroke-linecap="round"
               stroke-linejoin="round"
-              stroke-width="2"
+              stroke-width="{2}"
               d="M16 7a4 4 0 11-8 0 4 4 0 018 0zM12 14a7 7 0 00-7 7h14a7 7 0 00-7-7z"
             />
           </svg>
         </nuxt-link>
       </li>
       <!-- Items Timeline -->
-      <li class="hover:bg-indigo-100">
+      <li>
         <nuxt-link
           to="/profile/timeline"
-          class="h-16 px-6 flex justify-center items-center w-full focus:text-indigo-500"
+          class="h-16 px-6 flex justify-center items-center w-full focus:text-gray-500"
         >
           <svg
-            class="h-5 w-5"
-            width="24"
-            height="24"
+            class="nav"
             xmlns="http://www.w3.org/2000/svg"
             viewBox="0 0 20 20"
             fill="currentColor"
@@ -53,25 +60,24 @@
         </nuxt-link>
       </li>
       <!-- Items Project -->
-      <li class="hover:bg-indigo-100">
+      <li>
         <nuxt-link
           to="/profile/timeline"
-          class="h-16 px-6 flex justify-center items-center w-full focus:text-indigo-500"
+          class="h-16 px-6 flex justify-center items-center w-full focus:text-gray-500"
         >
           <svg
-            class="h-5 w-5"
-            width="24"
-            height="24"
+            class="nav"
             xmlns="http://www.w3.org/2000/svg"
-            fill="none"
-            viewBox="0 0 24 24"
-            stroke="currentColor"
+            viewBox="0 0 20 20"
+            fill="currentColor"
           >
             <path
-              stroke-linecap="round"
-              stroke-linejoin="round"
-              stroke-width="2"
-              d="M21 13.255A23.931 23.931 0 0112 15c-3.183 0-6.22-.62-9-1.745M16 6V4a2 2 0 00-2-2h-4a2 2 0 00-2 2v2m4 6h.01M5 20h14a2 2 0 002-2V8a2 2 0 00-2-2H5a2 2 0 00-2 2v10a2 2 0 002 2z"
+              fill-rule="evenodd"
+              d="M6 6V5a3 3 0 013-3h2a3 3 0 013 3v1h2a2 2 0 012 2v3.57A22.952 22.952 0 0110 13a22.95 22.95 0 01-8-1.43V8a2 2 0 012-2h2zm2-1a1 1 0 011-1h2a1 1 0 011 1v1H8V5zm1 5a1 1 0 011-1h.01a1 1 0 110 2H10a1 1 0 01-1-1z"
+              clip-rule="evenodd"
+            />
+            <path
+              d="M2 13.692V16a2 2 0 002 2h12a2 2 0 002-2v-2.308A24.974 24.974 0 0110 15c-2.796 0-5.487-.46-8-1.308z"
             />
           </svg>
         </nuxt-link>
@@ -80,23 +86,17 @@
 
     <div class="mt-auto h-16 flex items-center w-full">
       <!-- Action Download -->
-      <button
-        class="h-16 mx-auto flex justify-center items-center w-16 focus:text-indigo-500 hover:bg-indigo-100 focus:outline-none"
-      >
+      <button class="h-16 mx-auto flex justify-center items-center w-16">
         <svg
-          class="h-5 w-5 text-blue-700"
-          width="24"
-          height="24"
+          class="nav"
           xmlns="http://www.w3.org/2000/svg"
-          fill="none"
-          viewBox="0 0 24 24"
-          stroke="currentColor"
+          viewBox="0 0 20 20"
+          fill="currentColor"
         >
           <path
-            stroke-linecap="round"
-            stroke-linejoin="round"
-            stroke-width="2"
-            d="M4 16v1a3 3 0 003 3h10a3 3 0 003-3v-1m-4-4l-4 4m0 0l-4-4m4 4V4"
+            fill-rule="evenodd"
+            d="M3 17a1 1 0 011-1h12a1 1 0 110 2H4a1 1 0 01-1-1zm3.293-7.707a1 1 0 011.414 0L9 10.586V3a1 1 0 112 0v7.586l1.293-1.293a1 1 0 111.414 1.414l-3 3a1 1 0 01-1.414 0l-3-3a1 1 0 010-1.414z"
+            clip-rule="evenodd"
           />
         </svg>
       </button>
@@ -104,3 +104,12 @@
   </aside>
   <!-- </div> -->
 </template>
+
+<style lang="postcss" scoped>
+.nav {
+  @apply h-6 fill-current text-gray-900;
+}
+.nav:hover {
+  @apply h-8 bg-gray-900 text-white p-2 rounded-md fill-current;
+}
+</style>
