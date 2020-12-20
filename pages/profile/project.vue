bbd48ae4caf215af206a09776923e9c25d1e5a78
Ibrohim Hanifa <31683290+ibrohimhanifa@users.noreply.github.com>
2020-12-20 20:49:00 +0700

    Project

--- pages/profile/project.vue
+++ pages/profile/project.vue
@@ -10,20 +10,18 @@
             <h1 class="sm:text-3xl text-2xl font-bold title-font mb-2 text-gray-900">Project</h1>
           </div>
           <div class="flex flex-wrap sm:-m-4 -mx-4 -mb-10 -mt-4">
-            <div class="p-4 md:w-1/3 md:mb-0 mb-6 flex flex-col justify-center items-center max-w-sm mx-auto" v-for="project in projects" :key="project">
-              <div class="bg-gray-300 h-56 w-full rounded-lg shadow-md bg-cover bg-center" style="background-image: url(https://images.unsplash.com/photo-1521185496955-15097b20c5fe?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1951&q=80"></div>
-              <div class=" w-60 bg-white -mt-10 shadow-lg rounded-lg overflow-hidden p-5">
-
-                <div class="title-post font-bold">{{ project.name }}</div>
-                <div class="category-title flex-4 text-sm">({{ project.start }} - {{ project.end }})</div>
+            <div class="mb-6 flex flex-col justify-center items-center max-w-sm mx-auto" v-for="project in projects" :key="project">
+              <div class=" w-60 bg-white m-1 shadow-2xl rounded-lg overflow-hidden p-5">
+                <div class="title-post font-bold">{{ project.name }}</div>  
+                <div class="category-title flex-4 text-sm mb-2">({{ project.start }} - {{ project.end }})</div>
                 <div class="header-content inline-flex ">
-                  <div class="category-badge flex-1  h-4 w-4 m rounded-full m-1 bg-purple-100">
+                  <div class="category-badge flex-1  h-4 w-4 m rounded-full m-1 bg-white">
                     <div class="h-2 w-2 rounded-full m-1 bg-teal-500 " ></div>
                   </div>
                   <div class="category-title flex-4 text-sm">{{ project.role }}</div>
                 </div>
-                <div class="summary-post text-base">{{ project.desc }}</div>
-                <button class="bg-teal-500 text-white mt-4 block rounded p-2 text-sm "><span class="">{{project.categ}}</span></button>
+                <div class="summary-post text-base bg-teal-500 text-white block rounded p-2">{{ project.desc }}</div>
+                <div class= "p-2 text-sm "><span class="">{{project.categ}}</span></div>
               </div>
             </div>
           </div>
